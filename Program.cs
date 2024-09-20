90ff7be14b6e9b5f2357ac19f66c79813fdb7e96
DESKTOP-8O5FNSF\QWERTY <jabamba@csmbataan.ph.education>
2024-09-20 05:15:03 -0700

    Jelaine prelim

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Input values
+        Console.Write("Enter the principal amount: ");
+        double principal = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the rate of interest: ");
+        double rate = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the time (in years): ");
+        double time = Convert.ToDouble(Console.ReadLine());
+
+        // Simple Interest formula: SI = (P * R * T) / 100
+        double simpleInterest = (principal * rate * time) / 100;
+
+        // Output the result
+        Console.WriteLine("The Simple Interest is: " + simpleInterest);
+    }
+}
